7179ba5af203f6721093fb2e7450dbf2e77a2829
ElimuStuart <stuartelimu@gmail.com>
2020-01-23 08:55:40 +0300

    add NoteView

--- src/note/views.py
+++ src/note/views.py
@@ -1,3 +1,8 @@
 from rest_framework import viewsets
 from .serializers import NoteSerializer
 from .models import Note
+
+
+class NoteView(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
