110c0214c1cb09a41c73127e50c03664d96bbf5e
ElimuStuart <stuartelimu@gmail.com>
2020-02-25 23:23:33 +0300

    improve look and feel of navigation bar

--- frontend/src/NavigationBar.js
+++ frontend/src/NavigationBar.js
@@ -1,30 +1,46 @@
 import React from "react";
-import {Navbar, Nav, Button, Form, FormControl} from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Button,
+  Form,
+  FormControl,
+  Col,
+  InputGroup
+} from "react-bootstrap";
 import logo from "./logo.svg";
 
 function NavigationBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src={logo}
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />{' '}
-        Stue's Keep</Navbar.Brand>
+        <img
+          alt=""
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{" "}
+        Stue's Keep
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
+        <Form inline className="ml-5 pl-2">
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              placeholder="Search"
+              className="mr-sm-2"
+            />
+          </InputGroup>
+        </Form>
+
+        <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#link">Link</Nav.Link>
-          
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
     </Navbar>
   );
