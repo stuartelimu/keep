fccbebe9e63316cab19c79597859644914dfd3b3
ElimuStuart <stuartelimu@gmail.com>
2020-03-03 13:57:05 +0300

    add icons to card note

--- frontend/src/NoteItem.js
+++ frontend/src/NoteItem.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, ButtonGroup, ButtonToolbar, Button } from "react-bootstrap";
 
-const NoteItem = (props) => {
+const NoteItem = props => {
   return (
-    <Card onClick={() => props.renderSingleItem(props.id)}>
-      <Card.Body>
+    <Card>
+      <Card.Body onClick={() => props.renderSingleItem(props.id)}>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-          {props.body}
-        </Card.Text>
-        <Card.Text>
-          <small className="text-muted">Last updated {props.updated_at}</small>
-        </Card.Text>
+        <Card.Text>{props.body}</Card.Text>
       </Card.Body>
+      <Card.Footer>
+        <ButtonToolbar aria-label="Toolbar with button groups">
+          <ButtonGroup className="mr-auto" aria-label="First group">
+            <span className="action-button rounded-circle"><i class="fa fa-thumb-tack" aria-hidden="true"></i></span>
+            <span className="action-button rounded-circle"><i class="fa fa-eye-slash" aria-hidden="true"></i></span>
+            
+          </ButtonGroup>
+
+          
+
+          <ButtonGroup className="ml-auto" aria-label="Third group">
+            <span className="action-button rounded-circle"><i class="fa fa-trash" aria-hidden="true"></i></span>
+          </ButtonGroup>
+        </ButtonToolbar>
+      </Card.Footer>
     </Card>
   );
 };
