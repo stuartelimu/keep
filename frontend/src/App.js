93517bf99521b2d09cfb893d3381d2e28e1d897c
ElimuStuart <stuartelimu@gmail.com>
2020-03-03 18:36:06 +0300

    remove redundant code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,39 +7,6 @@ import { Container, Row, CardColumns } from "react-bootstrap";
 import CreateNote from "./CreateNote";
 import axios from "axios";
 
-const noteItems = [
-  {
-    id: 1,
-    title: "Go to Market",
-    body: "Buy ingredients to prepare dinner",
-    created_at: "2020-01-21T23:55:48.778899+03:00",
-    updated_at: "2020-01-21T23:55:48.778936+03:00"
-  },
-  {
-    id: 2,
-    title: "Study",
-    body: "Read Algebra and History textbook for upcoming test",
-    created_at: "2020-01-21T23:55:48.778899+03:00",
-    updated_at: "2020-01-21T23:55:48.778936+03:00"
-  },
-  {
-    id: 3,
-    title: "Sally's books",
-    body: "Go to library to rent sally's books",
-    created_at: "2020-01-21T23:55:48.778899+03:00",
-    updated_at: "2020-01-21T23:55:48.778936+03:00"
-  },
-  {
-    id: 4,
-    title: "Article",
-    body: "Write article on how to use django with react",
-    created_at: "2020-01-21T23:55:48.778899+03:00",
-    updated_at: "2020-01-21T23:55:48.778936+03:00"
-  }
-];
-
-let xhr;
-
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
 
@@ -54,10 +21,8 @@ class App extends Component {
       noteList: [],
       showModal: false,
       note: {
-        
         title: "",
-        body: "",
-        
+        body: ""
       }
     };
 
@@ -68,21 +33,17 @@ class App extends Component {
     this.refreshNoteList = this.refreshNoteList.bind(this);
   }
 
-  
   componentDidMount() {
     // xhr = new XMLHttpRequest();
     // xhr.open("GET", "http://127.0.0.1:8000/api/notes/", true);
     // xhr.send();
 
     // xhr.addEventListener("readystatechange", this.refreshNoteList, false);
-    
 
     this.refreshNoteList();
-
   }
 
   refreshNoteList() {
-
     // if (xhr.readyState === 4 && xhr.status === 200) {
     //   let response = JSON.parse(xhr.responseText);
 
@@ -93,10 +54,9 @@ class App extends Component {
     //   })
     // }
 
-    axios.get("/api/notes/")
-      .then(response => this.setState({noteList: response.data}))
-    
-
+    axios
+      .get("/api/notes/")
+      .then(response => this.setState({ noteList: response.data }));
   }
 
   handleChange(e) {
@@ -108,89 +68,37 @@ class App extends Component {
         [name]: value
       }
     });
-
-    console.log(this.state.note);
   }
 
   handleSubmit(e) {
     e.preventDefault();
 
     const savedNote = {
-      
       title: this.state.note.title,
-      body: this.state.note.body,
-      
+      body: this.state.note.body
     };
 
     const updatedNote = this.state.note;
 
-    const updatedNotes = this.state.noteList.map(note => {
-      if (note.id === updatedNote.id) {
-        note = { ...updatedNote };
-      }
-      return note;
-    });
-
     if (this.state.showModal) {
-
-      // this.setState({
-      //   noteList: updatedNotes,
-      //   showModal: false,
-      //   note: { id: "", title: "", body: "", updated_at: "" }
-      // })
-
-      axios.put(`/api/notes/${updatedNote.id}/`, updatedNote)
+      axios
+        .put(`/api/notes/${updatedNote.id}/`, updatedNote)
         .then(response => this.refreshNoteList())
-        .then(response => this.setState({note: {title: "", body: "" },
-        showModal: false}))
-
+        .then(response =>
+          this.setState({ note: { title: "", body: "" }, showModal: false })
+        );
     } else {
-      // let csrftoken = document.cookie.replace(/(?:(?:^|.*;\s*)csrftoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-      // console.log(savedNote);
-      // console.log(csrftoken)
-      // xhr.open("POST", "http://127.0.0.1:8000/api/notes/", true);
-      // xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      // xhr.setRequestHeader("X-CSRFToken", csrftoken);
-      // xhr.send(JSON.stringify(savedNote));
-
-      // xhr.addEventListener("readystatechange", this.refreshNoteList, false);
-      
-
-      axios.post("/api/notes/", savedNote)
+      axios
+        .post("/api/notes/", savedNote)
         .then(response => this.refreshNoteList())
-        .then(response => this.setState({note: {title: "", body: "" },
-                showCreate: false}))
-
+        .then(response =>
+          this.setState({ note: { title: "", body: "" }, showCreate: false })
+        );
     }
-    // this.state.showModal
-    //   ? this.setState({
-    //       noteList: updatedNotes,
-    //       showModal: false,
-    //       note: { id: "", title: "", body: "", updated_at: "" }
-    //     })
-    //   : this.setState(prevState => {
-    //       return {
-    //         noteList: prevState.noteList.concat(savedNote),
-    //         note: { id: "", title: "", body: "", updated_at: "" },
-    //         showCreate: false
-    //       };
-    //     });
-
-
-
-    console.log(this.state.noteList);
   }
 
   handleDelete(id) {
-    // const updatedNotes = this.state.noteList.filter(note => {
-    //   return note.id !== id;
-    // })
-
-    axios.delete(`/api/notes/${id}`)
-      .then(response => this.refreshNoteList())
-
-    // this.setState({noteList: updatedNotes});
-    // console.log(id);
+    axios.delete(`/api/notes/${id}`).then(response => this.refreshNoteList());
   }
 
   renderSingleItem = id => {
@@ -235,7 +143,9 @@ class App extends Component {
   render() {
     return (
       <>
-        <NavigationBar handleCreate={() => this.setState({showCreate: true})} />
+        <NavigationBar
+          handleCreate={() => this.setState({ showCreate: true })}
+        />
 
         <Container className="mt-5">
           <Row>
@@ -257,14 +167,6 @@ class App extends Component {
               handleChange={this.handleChange}
               handleSubmit={this.handleSubmit}
             />
-
-            {/* <ButtonToolbar>
-            <Button variant="primary" onClick={() => this.setState({modalShow: true})}>
-              Launch vertically centered modal
-            </Button>
-
-            
-          </ButtonToolbar> */}
           </Row>
         </Container>
       </>
