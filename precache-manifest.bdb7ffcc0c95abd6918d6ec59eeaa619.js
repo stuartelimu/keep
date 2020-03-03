568e6ccb65a7be8d9f22364e78968617141c42d4
ElimuStuart <stuartelimu@gmail.com>
2020-03-03 19:34:55 +0300

    Updates

---
+++ precache-manifest.bdb7ffcc0c95abd6918d6ec59eeaa619.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13a519a80519db4c7dd958099cd4295",
+    "url": "/keep/index.html"
+  },
+  {
+    "revision": "00e14f48acc82d926ac0",
+    "url": "/keep/static/css/2.8b575dec.chunk.css"
+  },
+  {
+    "revision": "10e6aa53739d0b2af634",
+    "url": "/keep/static/css/main.6d891b59.chunk.css"
+  },
+  {
+    "revision": "00e14f48acc82d926ac0",
+    "url": "/keep/static/js/2.fc440e8d.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/keep/static/js/2.fc440e8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10e6aa53739d0b2af634",
+    "url": "/keep/static/js/main.9897b2fc.chunk.js"
+  },
+  {
+    "revision": "c62bc67cf33bdec16b96",
+    "url": "/keep/static/js/runtime-main.89e93a0c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/keep/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/keep/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/keep/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/keep/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/keep/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/keep/static/media/logo.5d5d9eef.svg"
+  }
+]);
